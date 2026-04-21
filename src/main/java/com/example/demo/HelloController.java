1db855ddad600279c17337356d29a7e7d6fae5c1
athulrdwise <athul.r@lifetrenz.com>
2026-04-21 17:13:19 +0530

    message changed, deploy 2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World! 🚀";
+        return "Hello World! 🚀 This is 2nd Deployment";
     }
 }
