7b19dd8697cff923e5b3d51b986f63a6c45d691a
athulrdwise <athul.r@lifetrenz.com>
2026-04-23 12:45:35 +0530

    Trial push 1

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World, Good to see you!";
+        return "Hello World, Test PUSH";
     }
 }
