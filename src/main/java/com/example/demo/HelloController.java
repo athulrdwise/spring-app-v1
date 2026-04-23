fa32cd412087be312d80c7dfea4fc0c11186c53b
athulrdwise <athul.r@lifetrenz.com>
2026-04-23 14:12:51 +0530

    Trial push 3

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello ATHUL RETNAKAR, Welcome to Spring Boot!";
+        return "Hello ATHUL RETNAKAR, Welcome..!";
     }
 }
